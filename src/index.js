f3d5ff02322273fa63e0d84e9f7216c0c363ca90
Hasaan Mehmood <hasaan.mehmood.9@gmail.com>
2026-01-10 19:26:04 +0800

    Final

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const os = require('os');
 const app = express();
 const port = 80;
+const color = "blue";
+const hostname = os.hostname();
 
 app.get('/', (req, res) => {
-  res.send('<h1 style="color: blue;">Hello World!</h1>');
+  res.send(`<h1 style="color: ${color};">Hello World!</h1>
+<h2>Hostname: ${os.hostname()}</h2>`);
 });
 
+app.get('/api', (req, res) => {
+  const format = req.query.format;  // localhost/api?format=json
+  if (format === 'json') {
+    return res.json({ 
+      color,
+      hostname
+     });
+  } else {
+    return res.send(`COLOR: ${color}\nHOSTNAME: ${hostname}`);
+  }
+});
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
